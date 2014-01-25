e0c1e5df7e0f9a1ddd4562a9f6be3a7663c7326c
Stan Cristian Florin <contacto@stancristian.es>
2014-01-25 17:42:35 +0100

    init

---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour {
+
+	public GameObject Camara;
+	public GameObject mirror;
+	public GUISkin LifesGuiSkin;
+	public Texture lIconFull;
+	public Texture lIcon2Left;
+	public Texture lIcon1Left;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	void OnGUI() {
+		//GUI.Box (new Rect (-3,-3,100,30), lifes.ToString());
+		GUI.BeginGroup (new Rect (-3, -3, 150, 50));
+		GUI.Box (new Rect (0,0,50,50), lIconFull);
+		GUI.EndGroup ();
+
+	}
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Camara.transform.position);
+	}
+}
